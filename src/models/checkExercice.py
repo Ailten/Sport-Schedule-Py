96c380e25e0448f5d7775d9aba3db5b4db547107
Ailten <faouzi26b@live.fr>
2026-05-13 15:48:05 +0200

    service minor edit

--- src/models/checkExercice.py
+++ src/models/checkExercice.py
@@ -19,4 +19,4 @@ class CheckExercice(Base):
 
     # ---> parameters.
     date_check: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc))
-    check_count: Mapped[int] = mapped_column(default=1)
+    #check_count: Mapped[int] = mapped_column(default=1)
--- src/services/checkExercice.py
+++ src/services/checkExercice.py
@@ -1,8 +1,20 @@
 from service import Service
 from sqlalchemy.orm import Session
 from ..models.checkExercice import CheckExercice
+from ..models.exerciceToDo import ExerciceToDo
+from datetime import datetime
 
-class CheckExercice(Service):
+class CheckExerciceService(Service):
 
     def __init__(self, session_db: Session):
         super().__init__(session_db, CheckExercice)
+
+    
+    # create a checkExercice (or update), for an ExerciceToDo.
+    def checkAnExerciceToDo(self, exercice_to_do: ExerciceToDo, datetime_check: datetime|None=None) -> bool:
+        checkExercice = CheckExercice()
+        if datetime_check != None:
+            checkExercice.date_check = datetime_check
+        self.create(checkExercice)
+        
+        
