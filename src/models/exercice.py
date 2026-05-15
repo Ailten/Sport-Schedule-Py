19197a70297a52af971b975d9cfe62f1ed90d61b
Ailten <faouzi26b@live.fr>
2026-05-15 08:58:16 +0200

    edit minor

--- src/models/exercice.py
+++ src/models/exercice.py
@@ -10,3 +10,4 @@ class Exercice(BaseWithPK):
     url_image: Mapped[str] = mapped_column()
     purcentage_body_weight_use: Mapped[float] = mapped_column()
     amplitude: Mapped[float] = mapped_column()
+    is_minutes: Mapped[bool] = mapped_column()
