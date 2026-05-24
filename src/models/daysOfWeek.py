2f2f0df6f9504a5696be314bfcc60fb8bab2d0ec
Ailten <faouzi26b@live.fr>
2026-05-24 11:36:50 +0200

    days if week enum

---
+++ src/models/daysOfWeek.py
@@ -0,0 +1,10 @@
+from enum import Flag, auto
+
+class DaysOfWeek(Flag):
+    Monday=auto()
+    Tuesday=auto()
+    Wednesday=auto()
+    Thursday=auto()
+    Friday=auto()
+    Saturday=auto()
+    Sunday=auto()
